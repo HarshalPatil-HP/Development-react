3b3658c8e2d5e1174b19226b7c6a7f2e9e00ddfe
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-23 19:02:28 +0530

    checking the setup

--- react-app/src/practice.js
+++ react-app/src/practice.js
@@ -10,5 +10,17 @@
 // console.log(finalstr)
 
 let num=1234
+ console.log(num)
+let newnum=num.toString()
+console.log(newnum)
+let newstr=newnum.split('')
+console.log(newstr)
 
-num.toString()
+ let newnewstr=newstr.reverse()
+ console.log(newnewstr)
+
+ let finalstr=newnewstr.join('')
+ console.log(finalstr)
+
+ let finalnum=parseInt(finalstr)
+ console.log(finalnum)
