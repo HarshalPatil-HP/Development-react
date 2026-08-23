08fcd5612d5ee6a41d8e63d519d1320d1cc3773a
HarshalPatil-HP <chandrkantpatil080@gmail.com>
2026-08-23 19:03:33 +0530

    debugged

--- react-app/src/practice.js
+++ react-app/src/practice.js
@@ -9,18 +9,18 @@
 // let finalstr=newnewstr.join('')
 // console.log(finalstr)
 
-let num=1234
- console.log(num)
-let newnum=num.toString()
-console.log(newnum)
-let newstr=newnum.split('')
-console.log(newstr)
+// let num=1234
+//  console.log(num)
+// let newnum=num.toString()
+// console.log(newnum)
+// let newstr=newnum.split('')
+// console.log(newstr)
 
- let newnewstr=newstr.reverse()
- console.log(newnewstr)
+//  let newnewstr=newstr.reverse()
+//  console.log(newnewstr)
 
- let finalstr=newnewstr.join('')
- console.log(finalstr)
+//  let finalstr=newnewstr.join('')
+//  console.log(finalstr)
 
- let finalnum=parseInt(finalstr)
- console.log(finalnum)
+//  let finalnum=parseInt(finalstr)
+//  console.log(finalnum)
